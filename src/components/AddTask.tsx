8e0676e6a7cb226c90f51d17e2176dc1ec07560c
Roberto Augusto <robertogutonunes@gmail.com>
2025-01-11 16:57:13 -0300

    Feat: Adiciona component de `AddTask`
    - Add component
    - Add style to the component
    - Changes the :focus color of elements
    - Change body color

---
+++ src/components/AddTask.tsx
@@ -0,0 +1,15 @@
+import { PlusCircle } from "@phosphor-icons/react";
+
+import style from './AddTask.module.css';
+
+export function AddTask() {
+  return ( 
+    <div className={style.boxInputTaskAndCreate}>
+      <input type="text" placeholder='Adicionar uma nova tarefa' />
+      <button type='button'>
+        <strong>Criar</strong>
+        <PlusCircle size={16} />
+      </button>
+    </div>
+  );
+}
