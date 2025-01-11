7ee3d7fc4f336ce681843807628025b76d4c4b6c
Roberto Augusto <robertogutonunes@gmail.com>
2025-01-11 16:47:18 -0300

    Feat: Add container and change font size on mobile
    - Adds a container that holds the components
    - Add media that changes the overall font size of the application, as all elements use 'rem' as a measurement attribute

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { Header } from './components/Header';
 
 import style from './App.module.css';
 import './global.css';
+import { AddTask } from './components/AddTask';
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={style.container}>
+          <AddTask />
+      </div>
     </div>
   )
 }
